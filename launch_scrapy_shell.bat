e8060d8e476713f8d70927134e5bfe3d019833d0
Antoine Brès <antoine.bres@soprasteria.com>
2017-08-17 17:58:01 +0200

    Launch file

--- launch_scrapy_shell.bat
+++ launch_scrapy_shell.bat
@@ -1,13 +1,8 @@
 @echo off
 if "%1" == "" GOTO start
-if "%1" == "elasticsearch" GOTO elasticsearch
 if "%1" == "scrapy" GOTO scrapy
 :start
     start cmd /k launch_env scrapy
-    start cmd /k launch_env elasticsearch
-    GOTO end
-:elasticsearch
-    src/elasticsearch/bin/elasticsearch
     GOTO end
 :scrapy
     call activate scrapy_es_watson
